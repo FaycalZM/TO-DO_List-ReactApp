01a36cd3a9f76d10b94bd53d11d44eccf232c7b7
FaycalZM <zemmourisissy@gmail.com>
2021-08-16 16:29:55 +0100

    Updates

---
+++ precache-manifest.04328d9df82e1510925662a298dce36d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba4b8bfc41121bc618613729b056f02",
+    "url": "/TO-DO_List-ReactApp/index.html"
+  },
+  {
+    "revision": "83cb27ea213cd198ae9c",
+    "url": "/TO-DO_List-ReactApp/static/css/main.dc4e02dd.chunk.css"
+  },
+  {
+    "revision": "a5640d4bcbc7f4184abc",
+    "url": "/TO-DO_List-ReactApp/static/js/2.79ac4408.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TO-DO_List-ReactApp/static/js/2.79ac4408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83cb27ea213cd198ae9c",
+    "url": "/TO-DO_List-ReactApp/static/js/main.5b8adaab.chunk.js"
+  },
+  {
+    "revision": "6e580377d7432852a2be",
+    "url": "/TO-DO_List-ReactApp/static/js/runtime-main.9106f24f.js"
+  }
+]);
